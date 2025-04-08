58cbdb7603c7bbdfa60a78a2999c8ad2ab1dab68
Aman Kumar <aman228115918@gmail.com>
2025-04-08 10:00:19 +0530

    login update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
-
 import Login from "./components/Login";
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StudentDashboard from "./components/StudentDashboard";
-
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login/>}></Route>
-      <Route path="/student/home" element={<StudentDashboard/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/student/home" element={<StudentDashboard />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
